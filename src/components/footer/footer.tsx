f7b1bb673f8e3bf547159f4e292e8cff9403ad02
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-17 22:27:57 -0300

    feat: add attributes to footer component to test it

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ import './footer.styles.scss';
 
 function Footer() {
 	return (
-		<footer>
+		<footer data-testid='footer-component'>
 			<p>Data provided by Marvel. © 2022 MARVEL</p>
 		</footer>
 	);
