4f31b1f7f8912ef7a5408015339c4cab135fa4a1
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-18 13:43:22 -0300

    test: add new mocks for all responses

--- src/mocks/mocked-response.ts
+++ src/mocks/mocked-response.ts
@@ -1,7 +1,8 @@
-import { charactersMock } from './characters-mock';
+import { mockcharacters } from './mock-characters';
+
 export const mockedResponse = {
 	ok: true,
 	status: 200,
 	statusText: 'OK',
-	json: () => Promise.resolve(charactersMock),
+	json: () => Promise.resolve(mockcharacters),
 };
