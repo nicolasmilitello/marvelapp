073a5f70de09f46e89bb1bab36a1414a0b7c2821
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-17 22:42:08 -0300

    test: add tests for bookmark page

---
+++ src/views/bookmarks-page/__tests__/bookmarks-page.test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react';
+import insertAWrapper from '../../../utils/test-utils/insert-a-wrapper';
+import BookmarksPage from '../bookmarks-page';
+
+describe('<BookmarksPage />', () => {
+	beforeEach(() => {
+		insertAWrapper(<BookmarksPage />);
+	});
+
+	it('should render a <RestartButton /> component', async () => {
+		expect(
+			screen.getByTestId('restart-button-component')
+		).toBeInTheDocument();
+	});
+
+	it('should render three <BookmarkAndHiddenResourcesCollection /> components', async () => {
+		expect(
+			screen.getAllByTestId(
+				'bookmark-and-hidden-resources-collection-component'
+			)
+		).toHaveLength(3);
+		expect(screen.getAllByText(/characters/i)[0]).toBeInTheDocument();
+		expect(screen.getAllByText(/comics/i)[0]).toBeInTheDocument();
+		expect(screen.getAllByText(/stories/i)[0]).toBeInTheDocument();
+	});
+
+	it('should render a <Footer /> component', async () => {
+		expect(screen.getByTestId('footer-component')).toBeInTheDocument();
+	});
+});
