2657f5c438d5a69375337c5ad3a6adb8ebe15ce0
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-18 13:43:22 -0300

    test: add new mocks for all responses

---
+++ src/mocks/mock-single-story.ts
@@ -0,0 +1,78 @@
+export const mocksinglestory = {
+	code: 200,
+	status: 'Ok',
+	copyright: '© 2022 MARVEL',
+	attributionText: 'Data provided by Marvel. © 2022 MARVEL',
+	attributionHTML:
+		'<a href="http://marvel.com">Data provided by Marvel. © 2022 MARVEL</a>',
+	etag: 'a3a21625e688a06541c6b6b9719bf61328ef3c98',
+	data: {
+		offset: 0,
+		limit: 20,
+		total: 1,
+		count: 1,
+		results: [
+			{
+				id: 8,
+				title: 'In the wake of September 11th, the world watched as firefighters, police officers and EMT workers selflessly risked their lives',
+				description: '',
+				resourceURI: 'http://gateway.marvel.com/v1/public/stories/8',
+				type: 'story',
+				modified: '1969-12-31T19:00:00-0500',
+				thumbnail: null,
+				creators: {
+					available: 0,
+					collectionURI:
+						'http://gateway.marvel.com/v1/public/stories/8/creators',
+					items: [],
+					returned: 0,
+				},
+				characters: {
+					available: 0,
+					collectionURI:
+						'http://gateway.marvel.com/v1/public/stories/8/characters',
+					items: [],
+					returned: 0,
+				},
+				series: {
+					available: 1,
+					collectionURI:
+						'http://gateway.marvel.com/v1/public/stories/8/series',
+					items: [
+						{
+							resourceURI:
+								'http://gateway.marvel.com/v1/public/series/7',
+							name: 'THE CALL OF DUTY VOL. 1: THE BROTHERHOOD AND THE WAGON TPB (2002)',
+						},
+					],
+					returned: 1,
+				},
+				comics: {
+					available: 1,
+					collectionURI:
+						'http://gateway.marvel.com/v1/public/stories/8/comics',
+					items: [
+						{
+							resourceURI:
+								'http://gateway.marvel.com/v1/public/comics/942',
+							name: 'THE CALL OF DUTY VOL. 1: THE BROTHERHOOD AND THE WAGON TPB (Trade Paperback)',
+						},
+					],
+					returned: 1,
+				},
+				events: {
+					available: 0,
+					collectionURI:
+						'http://gateway.marvel.com/v1/public/stories/8/events',
+					items: [],
+					returned: 0,
+				},
+				originalIssue: {
+					resourceURI:
+						'http://gateway.marvel.com/v1/public/comics/942',
+					name: 'THE CALL OF DUTY VOL. 1: THE BROTHERHOOD AND THE WAGON TPB (Trade Paperback)',
+				},
+			},
+		],
+	},
+};
