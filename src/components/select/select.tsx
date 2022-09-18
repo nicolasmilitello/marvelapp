c5ab9aa87e3adcf85f4641c038e61911db4261f4
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-17 23:43:17 -0300

    feat: add attributes to select component to test it

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -59,6 +59,7 @@ const Select = ({
 				name='select'
 				defaultValue={'DEFAULT'}
 				onChange={(e) => handleSelectChange(e)}
+				data-testid="select-element"
 			>
 				<option value='DEFAULT' disabled>
 					{placeholder}
