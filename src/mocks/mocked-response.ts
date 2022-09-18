f8da275e48a10265eac66f0612f3628853398f47
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-09-18 13:53:35 -0300

    test: delete unnecessary mock

--- src/mocks/mocked-response.ts
+++
@@ -1,8 +0,0 @@
-import { mockcharacters } from './mock-characters';
-
-export const mockedResponse = {
-	ok: true,
-	status: 200,
-	statusText: 'OK',
-	json: () => Promise.resolve(mockcharacters),
-};
