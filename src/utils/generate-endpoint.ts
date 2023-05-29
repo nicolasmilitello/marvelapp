4c73d94d0f11ade43bf14a9d5baf2924083bd11d
nicomilitello <nmilitello@applaudostudios.com>
2023-05-29 16:51:14 -0300

    refactor: create .env file for sensitive data

--- src/utils/generate-endpoint.ts
+++ src/utils/generate-endpoint.ts
@@ -1,22 +1,22 @@
 //* constants
-import { endpoint } from '../constants/endpoint';
-import apiKey from '../constants/api-key';
-import resourcesPerPage from '../constants/number-resources-per-page';
+import { endpoint } from "../constants/endpoint";
+import apiKey from "../constants/endpoint-with-api-key";
+import resourcesPerPage from "../constants/number-resources-per-page";
 
 //* interfaces
-import FilterParamsType from '../interfaces/filter-params-type';
+import FilterParamsType from "../interfaces/filter-params-type";
 
 const generateEndpoint = (queries: FilterParamsType, type: string) => {
-	let urlApi = `${endpoint}/${type}?${apiKey}&limit=${resourcesPerPage}`;
+  let urlApi = `${endpoint}/${type}?${apiKey}&limit=${resourcesPerPage}`;
 
-	for (const query in queries) {
-		if (queries[query as keyof FilterParamsType]?.value) {
-			urlApi = `${urlApi}&${
-				queries[query as keyof FilterParamsType]?.query_param
-			}=${queries[query as keyof FilterParamsType]?.value}`;
-		}
-	}
-	return urlApi;
+  for (const query in queries) {
+    if (queries[query as keyof FilterParamsType]?.value) {
+      urlApi = `${urlApi}&${
+        queries[query as keyof FilterParamsType]?.query_param
+      }=${queries[query as keyof FilterParamsType]?.value}`;
+    }
+  }
+  return urlApi;
 };
 
 export default generateEndpoint;
--- src/views/characters-page/character-page.tsx
+++ src/views/characters-page/character-page.tsx
@@ -1,126 +1,126 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 
 //* components
 import {
-	Collection,
-	Footer,
-	Loading,
-	Pagination,
-	SearchBar,
-	SelectFilter,
-} from '../../components/index';
+  Collection,
+  Footer,
+  Loading,
+  Pagination,
+  SearchBar,
+  SelectFilter,
+} from "../../components/index";
 
 //* constants
-import { endpoint } from '../../constants/endpoint';
-import apiKey from '../../constants/api-key';
-import resourcesPerPage from '../../constants/number-resources-per-page';
-import { filtersCharactersPage } from '../../constants/filters-characters-page';
+import { endpoint } from "../../constants/endpoint";
+import apiKey from "../../constants/endpoint-with-api-key";
+import resourcesPerPage from "../../constants/number-resources-per-page";
+import { filtersCharactersPage } from "../../constants/filters-characters-page";
 
 //* context
-import { AppContext } from '../../context/context';
+import { AppContext } from "../../context/context";
 
 //* custom hooks
-import useFetch from '../../hooks/useFetch';
+import useFetch from "../../hooks/useFetch";
 
 //* interfaces
-import { CharactersResponse } from '../../interfaces/characters-response-api';
-import FilterParamsType from '../../interfaces/filter-params-type';
+import { CharactersResponse } from "../../interfaces/characters-response-api";
+import FilterParamsType from "../../interfaces/filter-params-type";
 
 //* styles
-import './character-page.styles.scss';
+import "./character-page.styles.scss";
 
 //* utils
-import generateEndpoint from '../../utils/generate-endpoint';
+import generateEndpoint from "../../utils/generate-endpoint";
 
 const CharacterPage = () => {
-	const { state, dispatch } = useContext(AppContext);
+  const { state, dispatch } = useContext(AppContext);
 
-	// pagination
-	const [currentPage, setCurrentPage] = useState(1);
+  // pagination
+  const [currentPage, setCurrentPage] = useState(1);
 
-	const [generatedEndpoint, setGeneratedEndpoint] = useState<string>(
-		`${endpoint}/characters?${apiKey}&limit=${resourcesPerPage}`
-	);
+  const [generatedEndpoint, setGeneratedEndpoint] = useState<string>(
+    `${endpoint}/characters?${apiKey}&limit=${resourcesPerPage}`
+  );
 
-	const [charactersFilterParams, setCharactersFilterParams] =
-		useState<FilterParamsType>(filtersCharactersPage);
+  const [charactersFilterParams, setCharactersFilterParams] =
+    useState<FilterParamsType>(filtersCharactersPage);
 
-	const { data, error, loading } =
-		useFetch<CharactersResponse>(generatedEndpoint);
+  const { data, error, loading } =
+    useFetch<CharactersResponse>(generatedEndpoint);
 
-	useEffect(() => {
-		setGeneratedEndpoint(
-			generateEndpoint(charactersFilterParams, 'characters')
-		);
+  useEffect(() => {
+    setGeneratedEndpoint(
+      generateEndpoint(charactersFilterParams, "characters")
+    );
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [
-		charactersFilterParams.search.value,
-		charactersFilterParams.pagination.value,
-		charactersFilterParams.comic?.value,
-		charactersFilterParams.story?.value,
-	]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    charactersFilterParams.search.value,
+    charactersFilterParams.pagination.value,
+    charactersFilterParams.comic?.value,
+    charactersFilterParams.story?.value,
+  ]);
 
-	useEffect(() => {
-		if (data) {
-			dispatch({ type: 'SET_CHARACTERS', payload: data.data?.results });
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [data]);
+  useEffect(() => {
+    if (data) {
+      dispatch({ type: "SET_CHARACTERS", payload: data.data?.results });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
 
-	return (
-		<div className='characterPageContainer' data-testid='characters-page'>
-			<div className='characterPageContainer__filterContainer'>
-				<SelectFilter
-					setCurrentPage={setCurrentPage}
-					params={charactersFilterParams}
-					handleParams={setCharactersFilterParams}
-					options={state.comicsOptions}
-					title='Filter by comic'
-					type='comic'
-				/>
+  return (
+    <div className="characterPageContainer" data-testid="characters-page">
+      <div className="characterPageContainer__filterContainer">
+        <SelectFilter
+          setCurrentPage={setCurrentPage}
+          params={charactersFilterParams}
+          handleParams={setCharactersFilterParams}
+          options={state.comicsOptions}
+          title="Filter by comic"
+          type="comic"
+        />
 
-				<SelectFilter
-					setCurrentPage={setCurrentPage}
-					params={charactersFilterParams}
-					handleParams={setCharactersFilterParams}
-					options={state.storiesOptions}
-					title='Filter by story'
-					type='story'
-				/>
+        <SelectFilter
+          setCurrentPage={setCurrentPage}
+          params={charactersFilterParams}
+          handleParams={setCharactersFilterParams}
+          options={state.storiesOptions}
+          title="Filter by story"
+          type="story"
+        />
 
-				<SearchBar
-					setCurrentPage={setCurrentPage}
-					params={charactersFilterParams}
-					handleParams={setCharactersFilterParams}
-					placeholder='Search by character name'
-				/>
-			</div>
+        <SearchBar
+          setCurrentPage={setCurrentPage}
+          params={charactersFilterParams}
+          handleParams={setCharactersFilterParams}
+          placeholder="Search by character name"
+        />
+      </div>
 
-			{loading && <Loading />}
+      {loading && <Loading />}
 
-			{error && (
-				<p className='charactersPageContainer__error'>{`Sorry, an error ocurred. ${error.status}: ${error.message}`}</p>
-			)}
+      {error && (
+        <p className="charactersPageContainer__error">{`Sorry, an error ocurred. ${error.status}: ${error.message}`}</p>
+      )}
 
-			{!loading && !error && (
-				<Collection loading={loading} typeOfContent='characters' />
-			)}
+      {!loading && !error && (
+        <Collection loading={loading} typeOfContent="characters" />
+      )}
 
-			{state.characters?.length ? (
-				<Pagination
-					params={charactersFilterParams}
-					handleParams={setCharactersFilterParams}
-					setCurrentPage={setCurrentPage}
-					currentPage={currentPage}
-					cardsPerPage={resourcesPerPage}
-					totalCards={data?.data.total}
-				/>
-			) : null}
+      {state.characters?.length ? (
+        <Pagination
+          params={charactersFilterParams}
+          handleParams={setCharactersFilterParams}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+          cardsPerPage={resourcesPerPage}
+          totalCards={data?.data.total}
+        />
+      ) : null}
 
-			<Footer />
-		</div>
-	);
+      <Footer />
+    </div>
+  );
 };
 
 export default CharacterPage;
